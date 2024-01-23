3fcbde589ed0887ed1314ba90b50445c5cbd8c2d
kevismatry <ismaelmaxi99@gmail.com>
2024-01-23 19:38:29 -0300

    esta empezado pero le falta terminar mi portfolio en angular 17

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      'custom': ['coolvetica', 'sans-serif']
+    },
+  },
+  plugins: [],
+}
+
